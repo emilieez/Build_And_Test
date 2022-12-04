f70e6fec397c05629221b8eaca5635fdbb7f7682
emilieez <emilie.m.zhang@gmail.com>
2022-12-03 19:57:46 -0800

    Better test folder structure

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -4,7 +4,7 @@ import { JSDOM } from 'jsdom'
 import fs from 'fs'
 import path from 'path'
 
-const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
+const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
 
 let dom
 let container
